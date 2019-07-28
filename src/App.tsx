7deef69ebf242c7a269451bc1fb740a76726ca30
kingdaro <kingdaro@gmail.com>
2019-07-27 19:46:46 -0500

    add keyboard avoiding vew

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react"
-import { View } from "react-native"
+import { KeyboardAvoidingView } from "react-native"
 import TrackList from "./track/TrackList"
 import { useStyles } from "./ui/style"
 
 export default function App() {
   const styles = useStyles()
   return (
-    <View style={[styles.background, styles.flex1]}>
+    <KeyboardAvoidingView style={[styles.background, styles.flex1]}>
       <TrackList />
-    </View>
+    </KeyboardAvoidingView>
   )
 }
