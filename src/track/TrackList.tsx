5019bef910818ae72aebbe59feafdf287384ab9d
kingdaro <kingdaro@gmail.com>
2019-07-27 21:03:41 -0500

    slightly cleaner track list pagination

--- src/track/TrackList.tsx
+++ src/track/TrackList.tsx
@@ -1,9 +1,20 @@
 import { gql } from "apollo-boost"
+import produce from "immer"
 import React from "react"
 import { useQuery } from "react-apollo"
 import { FlatList, TouchableOpacity, View } from "react-native"
 import { TrackListQuery, TrackListQueryVariables } from "../generated/graphql"
 import AppText from "../ui/AppText"
+
+type UpdateQueryOptions<Q, V = {}> = {
+  fetchMoreResult?: Q
+  variables?: V
+}
+
+type UpdateQueryFn<Q, V = {}> = (
+  prevResult: Q,
+  options: UpdateQueryOptions<Q, V>,
+) => Q
 
 export default function TrackList() {
   const { data, loading, refetch, fetchMore } = useQuery<
@@ -17,69 +28,69 @@ export default function TrackList() {
   const handleEndReached = () => {
     if (loading || !data) return
 
+    const updateQuery = produce<UpdateQueryFn<TrackListQuery>>(
+      (state, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return state
+        state.tracks.offset = fetchMoreResult.tracks.offset
+        state.tracks.rows.push(...fetchMoreResult.tracks.rows)
+        return state
+      },
+    )
+
     fetchMore({
       query: trackListQuery,
       variables: {
         limit: data.tracks.limit,
         offset: data.tracks.offset + data.tracks.limit,
       },
-      updateQuery(prevResult, { fetchMoreResult }) {
-        return {
-          ...prevResult,
-          tracks: {
-            ...prevResult.tracks,
-            offset: fetchMoreResult!.tracks.offset,
-            rows: [...prevResult.tracks.rows, ...fetchMoreResult!.tracks.rows],
-          },
-        }
-      },
+      updateQuery,
     })
   }
 
   const tracks = (data && data.tracks && data.tracks.rows) || []
 
   return (
-    <FlatList
-      data={tracks}
-      keyExtractor={(track) => String(track.id)}
-      contentContainerStyle={{
-        padding: 5,
-      }}
-      renderItem={({ item: track }) => (
-        <TouchableOpacity
-          style={{
-            backgroundColor: "rgb(13, 44, 76)",
-            padding: 10,
-            marginBottom: 5,
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <View
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={tracks}
+        keyExtractor={(track) => String(track.id)}
+        contentContainerStyle={{
+          padding: 5,
+        }}
+        renderItem={({ item: track }) => (
+          <TouchableOpacity
             style={{
-              width: 60,
-              height: 60,
-              marginRight: 10,
-              backgroundColor: "rgba(0, 0, 0, 0.2)",
-              borderRadius: 30,
+              backgroundColor: "rgb(13, 44, 76)",
+              padding: 10,
+              marginBottom: 5,
+              flexDirection: "row",
+              alignItems: "center",
             }}
-          />
-          <View style={{ flex: 1 }}>
-            <AppText numberOfLines={1} style={{ fontSize: 14, opacity: 0.7 }}>
-              {track.user.displayName}
-            </AppText>
-            <AppText numberOfLines={2}>{track.title}</AppText>
-          </View>
-        </TouchableOpacity>
-      )}
-      refreshing={loading}
-      onRefresh={() => refetch()}
-      onEndReached={handleEndReached}
-      onEndReachedThreshold={1}
-    />
-  )
+          >
+            <View
+              style={{
+                width: 60,
+                height: 60,
+                marginRight: 10,
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+              }}
+            />
 
-  // return <AppText>{JSON.stringify(data, null, 2)}</AppText>
+            <View style={{ flex: 1 }}>
+              <AppText numberOfLines={1} style={{ fontSize: 14, opacity: 0.7 }}>
+                {track.user.displayName}
+              </AppText>
+              <AppText numberOfLines={2}>{track.title}</AppText>
+            </View>
+          </TouchableOpacity>
+        )}
+        refreshing={loading}
+        onRefresh={() => refetch()}
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={1}
+      />
+    </View>
+  )
 }
 
 const trackListQuery = gql`
