e6e82e63a0c3c897f92d70049ebc910221a8c3e2
kingdaro <kingdaro@gmail.com>
2019-07-27 20:48:51 -0500

    add status bar spacer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react"
-import { KeyboardAvoidingView } from "react-native"
+import { KeyboardAvoidingView, StatusBar, View } from "react-native"
 import TrackList from "./track/TrackList"
 import { useStyles } from "./ui/style"
+
+function StatusBarSpacer() {
+  return <View style={{ height: StatusBar.currentHeight }} />
+}
 
 export default function App() {
   const styles = useStyles()
   return (
     <KeyboardAvoidingView style={[styles.background, styles.flex1]}>
+      <StatusBarSpacer />
       <TrackList />
     </KeyboardAvoidingView>
   )
